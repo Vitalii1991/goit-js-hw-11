0b5a5b034d10b634ab6ea8c1ea8ca0aa2d6c97d6
Vitalii1991 <ironwarrior1991@gmail.com>
2023-06-01 17:34:13 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,12 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
+
+new SimpleLightbox('.gallery');
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -15,4 +18,26 @@ function onFormSubmit(e) {
   const inputValue = searchQuery.value.toLowerCase();
 
   console.log(inputValue);
+}
+
+function createMarkup() {
+  const markup = `<div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+      </p>
+    </div>
+  </div>`;
+
+  refs.gallery.innerHTML = markup;
 }
