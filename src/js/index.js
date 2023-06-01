4bb0d39752b3d3cf0f8eb7ad767f3fc48570fc93
Vitalii1991 <ironwarrior1991@gmail.com>
2023-06-01 17:24:45 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { searchQuery } = e.currentTarget.elements;
+  const inputValue = searchQuery.value.toLowerCase();
+
+  console.log(inputValue);
+}
