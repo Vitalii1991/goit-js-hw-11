f1d1ffb671c9ac8e8864b83c715a1c7dcb85a6c1
Vitalii1991 <ironwarrior1991@gmail.com>
2023-06-02 10:05:54 +0300

    Create pixabay-api.js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '36965845-2e0bcc0d1faaf1681619d8a12';
+
+const params = {
+  key: API_KEY,
+  q: '',
+  image_type: photo,
+  orientation: horizontal,
+  safesearch: true,
+};
